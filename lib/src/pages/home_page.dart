356c3df8dd33a35fb593f5eadb86b21258cbb5ad
SM Shohag <fsm.cts@gmail.com>
2023-10-21 13:01:30 +0600

    fixed

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:forground_service_example/src/base/base.dart';
+import 'package:forground_service_example/src/helpers/klog.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,6 +13,24 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            TextButton(
+              onPressed: () async {
+                final x =
+                    await FlutterForegroundTask.checkNotificationPermission();
+
+                klog(x);
+              },
+              child: Text('Check'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final x =
+                    await FlutterForegroundTask.requestNotificationPermission();
+
+                klog(x);
+              },
+              child: Text('Request'),
+            ),
             IconButton(
               onPressed: () async {
                 Base.forgroundService.startForgroundService();
